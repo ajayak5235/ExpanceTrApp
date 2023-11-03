c5ddb3494d80031e2ef5c71b0deb5d97075be27a
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2023-11-04 00:14:21 +0530

    Delete util directory

--- util/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-const sequelize  = new Sequelize('college', 'root',process.env.PASSW_KEY,{
-    dialect: 'mysql' ,
-    host:'localhost'
-})
-module.exports = sequelize;
